3a9990f6ba7670f954f164d20837cff0cc918a50
gydoar <gydoar@hotmail.com>
2015-05-26 14:01:10 -0500

    agregando slide principal

--- footer.php
+++ footer.php
@@ -8,6 +8,7 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script src="<?php bloginfo('template_url' ); ?>/library/js/modernizr-custom-min.js"></script>
 <script src="<?php bloginfo('template_url' ); ?>/library/js/jquery.meanmenu.js"></script>
+<script src="<?php bloginfo('template_url' ); ?>/library/js/owl.carousel.js"></script>
 <!--/ script -->
 
 <!-- Scripts Adiconales -->
@@ -19,6 +20,30 @@
 			});
 		</script>
 	<!--/ Menu responsive -->
+
+		<script>
+			$(document).ready(function() {
+				 
+				  $("#owl-demo").owlCarousel({
+				 
+				      navigation : false, // Show next and prev buttons
+				      slideSpeed : 300,
+				      paginationSpeed : 400,
+				      singleItem:true,
+				      autoPlay: true,
+				      pagination: false
+				 
+				      // "singleItem:true" is a shortcut for:
+				      // items : 1, 
+				      // itemsDesktop : false,
+				      // itemsDesktopSmall : false,
+				      // itemsTablet: false,
+				      // itemsMobile : false
+				 
+				  });
+				 
+				});
+		</script>
 
 <!--/ Scripts Adiconales -->
 
--- front-page.php
+++ front-page.php
@@ -1,4 +1,13 @@
 <?php get_header(); ?>
 
+	<section class="body">
+
+		<div id="owl-demo" class="owl-carousel owl-theme">
+		  <div class="item"><img src="<?php bloginfo('template_url' ); ?>/library/img/banners/slide_01.png" alt="Banner1"></div>
+		  <div class="item"><img src="<?php bloginfo('template_url' ); ?>/library/img/banners/slide_02.png" alt="Banner2"></div>
+		  <div class="item"><img src="<?php bloginfo('template_url' ); ?>/library/img/banners/slide_03s.png" alt="Banner3"></div>		 
+		</div>
+
+	</section>
 
 <?php get_footer(); ?>
