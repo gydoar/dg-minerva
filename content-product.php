a4cddff2c6b2ce7bded8490ef8c8a2f2c0f5e6be
gydoar <gydoar@hotmail.com>
2015-05-28 18:15:18 -0500

    ultimos cambios del dia

--- content-product.php
+++ content-product.php
@@ -37,39 +37,40 @@ if ( 0 == ( $woocommerce_loop['loop'] - 1 ) % $woocommerce_loop['columns'] || 1 
 if ( 0 == $woocommerce_loop['loop'] % $woocommerce_loop['columns'] )
 	$classes[] = 'last';
 ?>
-<li <?php post_class( $classes ); ?>>
-
-	<?php do_action( 'woocommerce_before_shop_loop_item' ); ?>
 	
-		<div class="content-product">
+
+<div class="content-product three columns clearfix">
+
+	<li <?php post_class( $classes ); ?>>
+
+		<?php do_action( 'woocommerce_before_shop_loop_item' ); ?>
 			
-		
-			<a class="four columns" href="<?php the_permalink(); ?>">
+				<a href="<?php the_permalink(); ?>">
 
-				<?php
-					/**
-					 * woocommerce_before_shop_loop_item_title hook
-					 *
-					 * @hooked woocommerce_show_product_loop_sale_flash - 10
-					 * @hooked woocommerce_template_loop_product_thumbnail - 10
-					 */
-					do_action( 'woocommerce_before_shop_loop_item_title' );
-				?>
+					<?php
+						/**
+						 * woocommerce_before_shop_loop_item_title hook
+						 *
+						 * @hooked woocommerce_show_product_loop_sale_flash - 10
+						 * @hooked woocommerce_template_loop_product_thumbnail - 10
+						 */
+						do_action( 'woocommerce_before_shop_loop_item_title' );
+					?>
 
-				<h3 class="twelve columns"><?php the_title(); ?></h3>
+					<h3 class=""><?php the_title(); ?></h3>
+					<br>
+					<?php
+						/**
+						 * woocommerce_after_shop_loop_item_title hook
+						 *
+						 * @hooked woocommerce_template_loop_rating - 5
+						 * @hooked woocommerce_template_loop_price - 10
+						 */
+						do_action( 'woocommerce_after_shop_loop_item_title' );
+					?>
+					
+				</a>
 
-				<?php
-					/**
-					 * woocommerce_after_shop_loop_item_title hook
-					 *
-					 * @hooked woocommerce_template_loop_rating - 5
-					 * @hooked woocommerce_template_loop_price - 10
-					 */
-					do_action( 'woocommerce_after_shop_loop_item_title' );
-				?>
-				<br>
-			</a>
-
-		</div>
-	
-</li>
+			
+	</li>
+</div>
--- content-single-product.php
+++ content-single-product.php
@@ -61,18 +61,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 		?>
 
 	</div><!-- .summary -->
-	<div class="twelve columns">
-		<?php
-			/**
-			 * woocommerce_after_single_product_summary hook
-			 *
-			 * @hooked woocommerce_output_product_data_tabs - 10
-			 * @hooked woocommerce_upsell_display - 15
-			 * @hooked woocommerce_output_related_products - 20
-			 */
-			do_action( 'woocommerce_after_single_product_summary' );
-		?>
-	</div>
+	
 
 	<meta itemprop="url" content="<?php the_permalink(); ?>" />
 
